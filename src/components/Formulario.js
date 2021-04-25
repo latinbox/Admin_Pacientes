ab442d855f43e16d6834363f4ded28af6ae2cf11
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-25 09:44:39 -0400

    Actualizar Estado se actualiza cada vez que el usuario ingresa valores al form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+    // Crear State de citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+    //Funcion que se ejecuta cada que el usuario escribe un input
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+    //Extraer valores
+    
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
@@ -11,6 +28,7 @@ const Formulario = () => {
                  name="mascota"
                  className="u-full-width"
                  placeholder="Nombre mascota"
+                 onChange={actualizarState}
                 />
                  <label>Nombre Dueño</label>
                 <input 
@@ -18,23 +36,27 @@ const Formulario = () => {
                  name="mascota"
                  className="u-full-width"
                  placeholder="Nombre Dueño Mascota"
+                 onChange={actualizarState}
                 />
                  <label>Fecha</label>
                 <input 
                  type="date"
                  name="fecha"
                  className="u-full-width"
+                 onChange={actualizarState}
                 />
                 <label>Hora</label>
                 <input 
                  type="time"
                  name="hora"
                  className="u-full-width"
+                 onChange={actualizarState}
                 />
                 <label>Síntomas</label>
                 <textarea 
                  className="u-full-width"
                  name="sintomas"
+                 onChange={actualizarState}
                 />
                 <button
                     type="submit"
