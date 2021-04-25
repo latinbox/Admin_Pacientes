1fb30f0d72befa9167e6b839fceb553ca4e0913b
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-25 14:59:51 -0400

    localStorage & proptypes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 function App() {
-
+  // citas localstorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
-
+  const [citas, guardarCitas] = useState(citasIniciales);
+  // use effect para realizar operaciones cuando el state cambia
+  useEffect( () => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas]);
   //Funcion que tome las citas actuales y agregue una nueva. En react debes utilizar spread operator que modifica el state
   const crearCita = cita => {
     guardarCitas([ ...citas, cita ]);
   }
+
+   // mensaje condicional
+   const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas';
    // Función que elimina una cita por su id
    const eliminarCita = id => {
     const nuevasCitas = citas.filter(cita => cita.id !== id );
     guardarCitas(nuevasCitas);
  }
-  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas';
+  
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
